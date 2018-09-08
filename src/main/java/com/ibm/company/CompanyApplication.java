85c60bb11dddeb6c7cbd10d294fab388e0b979f7
saketkashyap <sakkashy@in.ibm.com>
2018-09-08 16:02:41 +0530

    Configured communication through Eureka Server

--- src/main/java/com/ibm/company/CompanyApplication.java
+++ src/main/java/com/ibm/company/CompanyApplication.java
@@ -2,8 +2,10 @@ package com.ibm.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompanyApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/ibm/company/CompanyApplicationTests.java
+++ src/test/java/com/ibm/company/CompanyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ibm.company;
+/*package com.ibm.company;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class CompanyApplicationTests {
 	}
 
 }
+*/
